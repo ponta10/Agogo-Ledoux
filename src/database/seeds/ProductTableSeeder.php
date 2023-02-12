0fe5332e041350426fcf73b2d44415608a31dd77
ponta10 <mponta2002@gmail.com>
2023-02-12 22:28:00 +0900

    productテーブル&seeder作成

---
+++ src/database/seeds/ProductTableSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $params = [
+            [
+                'name' => 'みかん',
+                'desc' => '愛媛のミカンだよ',
+                'image' => 'mikan.png',
+                'price' => 120,
+                'stock' => 5,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'name' => 'りんご',
+                'desc' => '青森のりんごだよ',
+                'image' => 'apple.png',
+                'price' => 150,
+                'stock' => 4,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'name' => 'ぶどう',
+                'desc' => '山梨の葡萄だよ',
+                'image' => 'grape.png',
+                'price' => 150,
+                'stock' => 4,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+            [
+                'name' => 'もも',
+                'desc' => '山梨のももだよ',
+                'image' => 'peach.png',
+                'price' => 200,
+                'stock' => 10,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+            ],
+        ];
+        foreach ($params as $param) {
+            DB::table('products')->insert($param);
+        }
+    }
+}
