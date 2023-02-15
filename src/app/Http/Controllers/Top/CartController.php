e53778fc6318fbe2feec486cc1087d0ffc4befdc
ponta10 <mponta2002@gmail.com>
2023-02-16 00:19:13 +0900

    controler階層変更

--- src/app/Http/Controllers/Top/CartController.php
+++ src/app/Http/Controllers/Top/CartController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Top;
+use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -20,29 +20,29 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin', 'as' => 'admin'], function () {
-    Route::get('/', 'AdminController@index');
+    Route::get('/', 'Admin\AdminController@index');
     Route::group(['prefix' => 'product', 'as' => '.product'], function () {
-        Route::get('/', 'ProductController@index');
-        Route::post('/store', 'ProductController@store')->name('.store');
-        Route::get('/destroy/{id}', 'ProductController@destroy')->name('.destroy');
-        Route::get('/restore/{id}', 'ProductController@restore')->name('.restore');
-        Route::get('/delete/{id}', 'ProductController@delete')->name('.delete');
-        Route::get('/show/{id}', 'ProductController@show')->name('.show');
-        Route::put('/edit/{id}', 'ProductController@edit')->name('.edit');
-        Route::get('/search', 'ProductController@search')->name('.search');
-        Route::get('/sort', 'ProductController@sort')->name('.sort');
+        Route::get('/', 'Admin\ProductController@index');
+        Route::post('/store', 'Admin\ProductController@store')->name('.store');
+        Route::get('/destroy/{id}', 'Admin\ProductController@destroy')->name('.destroy');
+        Route::get('/restore/{id}', 'Admin\ProductController@restore')->name('.restore');
+        Route::get('/delete/{id}', 'Admin\ProductController@delete')->name('.delete');
+        Route::get('/show/{id}', 'Admin\ProductController@show')->name('.show');
+        Route::put('/edit/{id}', 'Admin\ProductController@edit')->name('.edit');
+        Route::get('/search', 'Admin\ProductController@search')->name('.search');
+        Route::get('/sort', 'Admin\ProductController@sort')->name('.sort');
     });
-    Route::get('/userList', 'AdminController@userList')->name('.userList');
-    Route::get('/setting', 'AdminController@setting')->name('.setting');  //ルート名「product.show」
+    Route::get('/userList', 'Admin\AdminController@userList')->name('.userList');
+    Route::get('/setting', 'Admin\AdminController@setting')->name('.setting');  //ルート名「product.show」
 });
 
 Route::group(['prefix' => 'user', 'as' => 'user'], function(){
-    Route::get('/', 'UserController@index');    //ルート名「product」
-    Route::get('/home', 'UserController@index');  //ルート名「product.show」
-    Route::get('/userList', 'UserController@userList')->name('.userList');
-    Route::get('/setting', 'UserController@setting')->name('.setting');  //ルート名「product.show」
+    Route::get('/', 'Top\UserController@index');    //ルート名「product」
+    Route::get('/home', 'Top\UserController@index');  //ルート名「product.show」
+    Route::get('/userList', 'Top\UserController@userList')->name('.userList');
+    Route::get('/setting', 'Top\UserController@setting')->name('.setting');  //ルート名「product.show」
     Route::group(['prefix' => 'cart', 'as' => 'cart'], function () {
-        Route::get('/', 'CartController@index');
-        Route::get('/setting', 'AdminController@setting')->name('.setting');
+        Route::get('/', 'Top\CartController@index');
+        Route::get('/setting', 'Admin\AdminController@setting')->name('.setting');
     });
 });
