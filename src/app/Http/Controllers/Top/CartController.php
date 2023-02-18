9219ecf18496da9f70467e4a4afca8ca0d1ec656
keishiro terashita <20153180g@gmail.com>
2023-02-18 12:28:48 +0900

    カート表示機能 done

--- src/app/Http/Controllers/Top/CartController.php
+++ src/app/Http/Controllers/Top/CartController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers\Top;
+
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Cart;
+use App\Product;
 
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     //
-    public function index(){
-        return view('cart.show');
+    public function index()
+    {
+        $user_id = Auth::id();
+        $items = Cart::where('user_id', $user_id)->join('products', 'carts.product_id', '=', 'products.id')->get();
+        return view('cart.show', compact('items'));
     }
 }
