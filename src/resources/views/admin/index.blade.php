c4c3845818d146c5301dc63d4da225c42a16cdd9
ponta10 <mponta2002@gmail.com>
2023-02-20 01:07:48 +0900

    微修正

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -3,15 +3,18 @@
 <div class="home-container">
     <div class="top">
         <div class="sail">
-            <h2>売上状況</h2>
+            <h2>売り上げ状況</h2>
             <div class="sail-month">
+                <p class="title">今月の売り上げ</p>
                 <p><span>¥{{$profit_month}}</span>/<span>{{$count_month}}件</span></p>
             </div>
             <div class="sail-day">
                 <div class="sail-day_yesterday">
+                    <p class="title">昨日の売り上げ</p>
                     <p><span>¥{{$profit_yesterday}}</span>/<span>{{$count_yesterday}}件</span></p>
                 </div>
                 <div class="sail-day_today">
+                    <p class="title">今日の売り上げ</p>
                     <p><span>¥{{$profit_today}}</span>/<span>{{$count_today}}件</span></p>
                 </div>
             </div>
@@ -21,9 +24,11 @@
             <hr />
             <div class="shop-information">
                 <div class="stock">
+                    <p class="title">会員数</p>
                     <p>{{$user_count}}</p>
                 </div>
                 <div class="user">
+                    <p class="title">在庫なし商品数</p>
                     <p>{{$no_stock_product}}</p>
                 </div>
             </div>
